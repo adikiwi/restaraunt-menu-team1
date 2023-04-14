f3af20748d1fb591451accaae537d7dc09886607
erzhanmurasbekov <erzhanmurasbekov@gmail.com>
2023-04-13 18:22:34 -0700

    menu project

--- js/main.js
+++ js/main.js
@@ -72,16 +72,10 @@ function filter(word){
 }
 //////////////////////////////////////////////////////////////////
 
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("cartBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("cartBtn");
+const span = document.getElementsByClassName("close")[0];
 const content = document.querySelector(".namesOfDishes")
 btn.onclick = function() {
   modal.style.display = "block";
@@ -97,13 +91,11 @@ btn.onclick = function() {
   
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
   
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
